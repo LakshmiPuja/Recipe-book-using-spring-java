fa83b51d999f060c3f8f37cc308650e7e6df2b30
puja <puja@example.com>
2023-02-26 07:29:55 +0000

    Recipe Book

---
+++ src/main/java/com/example/recipe/RecipeRepository.java
@@ -0,0 +1,12 @@
+// Write your code here
+package com.example.recipe;
+
+import java.util.*;
+
+public interface RecipeRepository{
+    ArrayList<Recipe> getRecipes();
+    Recipe addRecipe(Recipe recipe);
+    Recipe getRecipe(int recipeId);
+    Recipe updateRecipe(int recipeId,Recipe recipe);
+    void deleteRecipe(int recipeId);
+}
